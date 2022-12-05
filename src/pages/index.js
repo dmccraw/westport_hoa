00664f230493a0d59826bf74f435132440385ced
Dustin McCraw <dustin.mccraw@gatsbyjs.com>
2022-12-04 20:16:08 -0800

    add documents

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,21 @@ import * as React from "react";
 
 export default function Home() {
   return (
-    <UL>
-      <Link to="static/Bylaws.pdf">Bylaws</Link>
-    </UL>
+    <ul>
+      <li>
+        <Link to="/documents/Bylaws.pdf">Bylaws</Link>
+      </li>
+      <li>
+        <Link to="/documents/CC&Rs.pdf">CCR's</Link>
+      </li>
+      <li>
+        <Link to="/documents/Community Handbook.pdf">Communite Handbook</Link>
+      </li>
+      <li>
+        <Link to="/documents/Patio Landscaping Guidelines.pdf">
+          Patio Landscaping Guidelines
+        </Link>
+      </li>
+    </ul>
   );
 }
